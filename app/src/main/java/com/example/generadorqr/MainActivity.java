49f3688b9812477905f288d51afcf9d4b714a2dd
Salvador20590284 <ramirezhernandezsalvador201@gmail.com>
2022-11-28 22:48:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/generadorqr/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.generadorqr;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView ivCodigoQR;
+    EditText etDatos;
+    Button btnGenerar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivCodigoQR = findViewById(R.id.ivCodigoQR);
+        etDatos = findViewById(R.id.etDatos);
+        btnGenerar = findViewById(R.id.btnGenerar);
+
+        btnGenerar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+                    /* DATOS PARA MANEJAR
+                     *  TELEFONO ES: tel:
+                     *  SMS ES: numero:mensaje
+                     *  MAIL: mailto
+                     * */
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.encodeBitmap("mailto:"+etDatos.getText().toString(), BarcodeFormat.QR_CODE, 750, 750);
+
+                    ivCodigoQR.setImageBitmap(bitmap);
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+
+                }
+            }
+        });
+    }
+}
